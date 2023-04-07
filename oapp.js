08c7dfe569af350bed7ada1b4e0d6c34d1ee498c
Sara11pee <129987737+Sara11pee@users.noreply.github.com>
2023-04-07 23:42:42 +0800

    Add files via upload

---
+++ oapp.js
@@ -0,0 +1,22 @@
+function PeeButt(){
+  document.getElementById("_warning").innerHTML = "You will NEVER escape pee :))))))"
+}
+const holder = document.getElementById('peeknows')
+holder.addEventListener('click', Clicklik)
+holder.addEventListener('mouseover', Obeber)
+holder.addEventListener('mouseout', MausAut)
+
+
+
+function Clicklik(){
+  holder.innerHTML = 'Why you cliklik *cri*'
+}
+
+function Obeber(){
+  holder.innerHTML = 'go away'
+}
+
+function MausAut(){
+  holder.innerHTML = "gud :))"
+}
+
